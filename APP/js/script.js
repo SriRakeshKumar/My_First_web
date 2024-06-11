055678f63da3b9acd8719274b38507579458b8cb
Rakesh.Kumar <srirakeshkumar891946@gmail.com>
2024-06-11 21:29:16 +0530

    mobile  view header bug fix ...

--- APP/js/script.js
+++ APP/js/script.js
@@ -1,18 +1,19 @@
-const overlay =document.querySelector('.overlay');
-const btnHamburger=document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
+const overlay = document.querySelector(".overlay");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const header2 = document.querySelector(".header__links");
 
-btnHamburger.addEventListener('click',function(){
-console.log('open hamburger');
-if(header.classList.contains('open')){
-    header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
-
-}
-else{
-    header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
-}
+btnHamburger.addEventListener("click", function () {
+  console.log("open hamburger");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+    header2.classList.add("hide-for-mobile");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+    header2.classList.remove("hide-for-mobile");
+  }
 });
